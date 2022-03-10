46e6506c4c19f72da4b78b20a67d6133c9e3af2a
Gerard Yap <cheekian.yap@aiesec.net>
2022-03-11 06:10:55 +0800

    first commit

---
+++ confinement_test.go
@@ -0,0 +1,51 @@
+package concurrencyingo_test
+
+import (
+	"bytes"
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestConfinement1(t *testing.T) {
+	chanOwner := func() <-chan int {
+		results := make(chan int, 5)
+		go func() {
+			defer close(results)
+			for i := 0; i <= 5; i++ {
+				results <- i
+			}
+		}()
+		return results
+	}
+
+	consumer := func(results <-chan int) {
+		for result := range results {
+			fmt.Printf("Received: %d\n", result)
+		}
+		fmt.Println("Done receiving!")
+	}
+
+	results := chanOwner()
+	consumer(results)
+}
+
+func TestConfinement2(t *testing.T) {
+	printData := func(wg *sync.WaitGroup, data []byte) {
+		defer wg.Done()
+
+		var buff bytes.Buffer
+		for _, b := range data {
+			fmt.Fprintf(&buff, "%c", b)
+		}
+		fmt.Println(buff.String())
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	data := []byte("golang")
+	go printData(&wg, data[:3])
+	go printData(&wg, data[3:])
+
+	wg.Wait()
+}
