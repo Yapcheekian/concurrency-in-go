923d3677d6397df4e75e3f55e7d617c32968bc90
Gerard Yap <cheekian.yap@aiesec.net>
2022-03-11 09:26:02 +0800

    add error handling pattern

---
+++ error_handling_test.go
@@ -0,0 +1,45 @@
+package concurrencyingo_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestErrorHandling(t *testing.T) {
+	type Result struct {
+		Error    error
+		Response *http.Response
+	}
+	checkStatus := func(done <-chan interface{}, urls ...string) <-chan Result {
+		results := make(chan Result)
+		go func() {
+			defer close(results)
+
+			for _, url := range urls {
+				var result Result
+				resp, err := http.Get(url)
+				result = Result{Error: err, Response: resp}
+				select {
+				case <-done:
+					return
+				case results <- result:
+				}
+			}
+		}()
+		return results
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	urls := []string{"https://www.google.com", "https://badhost"}
+	for result := range checkStatus(done, urls...) {
+		if result.Error != nil {
+
+			fmt.Printf("error: %v", result.Error)
+			continue
+		}
+		fmt.Printf("Response: %v\n", result.Response.Status)
+	}
+}
